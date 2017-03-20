086943a94dce0721439b3c637503efcb36091c32
Beata Szczygielska <beata.szczygielska@gmail.com>
2017-03-20 16:26:58 +0100

    Global var 'app' created

--- app.js
+++ app.js
@@ -1,45 +1,47 @@
 var app = {
     //...
+    templates: {},
     helper: {
-        compileHbsTemplate: function(selector) {
-
+        compileSingleHbsTemplate: function(selector) {
+            var source = $(selector).html();
+            return Handlebars.compile(source);
         },
         compileHbsTemplates: function() {
-
+            app.templates.homepageTemplate = app.helper.compileSingleHbsTemplate("#home");
+            app.templates.addBookFormTemplate = app.helper.compileSingleHbsTemplate("#add-book");
+            app.templates.wishListPageTemplate = app.helper.compileSingleHbsTemplate("#wish-list");
+        }
+    },
+    actions: {
+        displayHomepage: function() {
+            var context = {title: "Hello, book lovers!", subtitle: "Work in progress..."};
+            var homepageHtmlResult = app.templates.homepageTemplate(context);
+            $('#container').html(homepageHtmlResult);
+        },
+        displayAddBookForm: function() {
+            var context = {title: "Here will be a form for adding books."};
+            var addBookFormHtmlResult = app.templates.addBookFormTemplate(context);
+            $('#container').html(addBookFormHtmlResult);
+        },
+        displayWishListPage: function() {
+            var context = {title: "Here will be a list of books that I want to have."};
+            var wishListPageHtmlResult = app.templates.wishListPageTemplate(context);
+            $('#container').html(wishListPageHtmlResult);
+        }
+    },
+    eventHandlers: {
+        registerEventHandlers: function() {
+            $('#home-page').on('click', app.actions.displayHomepage);
+            $('#add-book-form').on('click', app.actions.displayAddBookForm);
+            $('#wish-list-page').on('click', app.actions.displayWishListPage);
         }
     },
     init: function() {
-        //...
+        app.helper.compileHbsTemplates();
+        app.eventHandlers.registerEventHandlers();
     }
 };
 
 $(document).ready(function() {
-    // app.init();
-    var source = $("#home").html();
-    var homepageTemplate = Handlebars.compile(source);
-    var context = {title: "Hello, book lovers!", subtitle: "Work in progress..."};
-    var homepageHtmlResult = homepageTemplate(context);
-
-    $('#home-page').on('click', function() {
-        $('#container').html(homepageHtmlResult);
-    });
-
-    var source = $("#add-book").html();
-    var addBookFormTemplate = Handlebars.compile(source);
-    var context = {title: "Here will be a form for adding books."};
-    var addBookFormHtmlResult = addBookFormTemplate(context);
-
-    $('#add-book-form').on('click', function() {
-        $('#container').html(addBookFormHtmlResult);
-    });
-
-    var source = $("#wish-list").html();
-    var wishListPageTemplate = Handlebars.compile(source);
-    var context = {title: "Here will be a list of books that I want to have."};
-    var wishListPageHtmlResult = wishListPageTemplate(context);
-
-    $('#wish-list-page').on('click', function() {
-        $('#container').html(wishListPageHtmlResult);
-    });
-
+    app.init();
 });
