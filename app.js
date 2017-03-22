9daf653dc7faa4b72e4e55084e6363553978b594
Beata Szczygielska <beata.szczygielska@gmail.com>
2017-03-22 18:29:32 +0100

    First (primitive) ajax made

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ var app = {
             app.templates.homepageTemplate = app.helper.compileSingleHbsTemplate("#home");
             app.templates.addBookFormTemplate = app.helper.compileSingleHbsTemplate("#add-book");
             app.templates.wishListPageTemplate = app.helper.compileSingleHbsTemplate("#wish-list");
+            app.templates.paperBooksTemplate = app.helper.compileSingleHbsTemplate('#paper-books');
+            app.templates.eBooksTemplate = app.helper.compileSingleHbsTemplate('#e-books');
+            app.templates.audiobooksTemplate = app.helper.compileSingleHbsTemplate('#audiobooks');
         }
     },
     actions: {
@@ -27,6 +30,27 @@ var app = {
             var context = {title: "Here will be a list of books that I want to have."};
             var wishListPageHtmlResult = app.templates.wishListPageTemplate(context);
             $('#container').html(wishListPageHtmlResult);
+        },
+        displayPaperBooks: function() {
+            $.get('http://localhost:3000/books?bookTypeId=1').then(function(response) {
+                $('#container').html(app.templates.paperBooksTemplate({
+                    paperBooks: response
+                }));
+            });
+        },
+        displayEbooks: function() {
+            $.get('http://localhost:3000/books?bookTypeId=2').then(function(response) {
+                $('#container').html(app.templates.eBooksTemplate({
+                    eBooks: response
+                }));
+            });
+        },
+        displayAudiobooks: function() {
+            $.get('http://localhost:3000/books?bookTypeId=3').then(function(response) {
+                $('#container').html(app.templates.audiobooksTemplate({
+                    audiobooks: response
+                }));
+            });
         }
     },
     eventHandlers: {
@@ -34,6 +58,9 @@ var app = {
             $('#home-page').on('click', app.actions.displayHomepage);
             $('#add-book-form').on('click', app.actions.displayAddBookForm);
             $('#wish-list-page').on('click', app.actions.displayWishListPage);
+            $('.dropdown-menu').on('click', '#paper-book-filter', app.actions.displayPaperBooks);
+            $('.dropdown-menu').on('click', '#e-book-filter', app.actions.displayEbooks);
+            $('.dropdown-menu').on('click', '#audiobook-filter', app.actions.displayAudiobooks);
         }
     },
     init: function() {
