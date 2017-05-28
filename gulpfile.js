5eb15cf7d970e011610c1e181d9ab31f9b76e468
Beata Szczygielska <beata.szczygielska@gmail.com>
2017-05-28 18:07:12 +0200

    gulp tasks for vendors js & css

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,44 @@ var handlebars = require('gulp-handlebars');
 var wrap = require('gulp-wrap');
 var declare = require('gulp-declare');
 
-var appJs = ['./src/app.js', './src/utils.js', './src/model.js']; // przypisuję do zmiennej, bo DRY :)
+var appJs = [ // przypisuję do zmiennej, bo DRY :)
+  './src/app.js', 
+  './src/utils.js', 
+  './src/model.js'
+];
+
+var vendorsJs = [
+  './node_modules/jquery/dist/jquery.js', 
+  './node_modules/bootstrap/dist/js/bootstrap.js', 
+  './node_modules/bootstrap-submenu/dist/js/bootstrap-submenu.js',
+  './node_modules/jquery-form-validator/form-validator/jquery.form-validator.js',
+  './node_modules/bootstrap-typeahead/js/bootstrap-typeahead.js', 
+  './node_modules/handlebars/dist/handlebars.js'
+];
+
+var vendorsCss = [
+  './node_modules/bootstrap/dist/css/bootstrap.css', 
+  './node_modules/bootstrap-submenu/dist/css/bootstrap-submenu.min.css', 
+  './node_modules/jquery-form-validator/form-validator/theme-default.css'
+];
+
 var templatesHbs = ['./src/templates/*.hbs'];
 
-gulp.task('scripts', function() {
+gulp.task('scripts-js', function() {
   return gulp.src(appJs)
     .pipe(concat('all.js'))
+    .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('vendors-js', function() {
+  return gulp.src(vendorsJs)
+    .pipe(concat('vendors.js'))
+    .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('vendors-css', function() {
+  return gulp.src(vendorsCss)
+    .pipe(concat('vendors.css'))
     .pipe(gulp.dest('./dist/'));
 });
 
@@ -28,8 +60,10 @@ gulp.task('templates', function(){
 });
 
 gulp.task('watch', ['default'], function() {
-    gulp.watch([appJs], ['scripts']);
+    gulp.watch([appJs], ['scripts-js']);
+    gulp.watch([vendorsJs], ['vendors-js']);
+    gulp.watch([vendorsCss], ['vendors-css']);
     gulp.watch([templatesHbs], ['templates']);
 });
 
-gulp.task('default', ['scripts', 'templates']);
+gulp.task('default', ['scripts-js', 'vendors-js', 'vendors-css', 'templates']);
